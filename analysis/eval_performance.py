7a6bed4aa1845062eed05fe3fd665f38a831a302
Cliff Kerr <questionably@gmail.com>
2026-02-25 02:19:30 -0500

    working on analysis script

--- analysis/eval_performance.py
+++ analysis/eval_performance.py
@@ -2,25 +2,37 @@
 Evaluate performance of different models/configurations.
 """
 
-# %%
 import numpy as np
 import sciris as sc
 from rich import print
 import matplotlib.pyplot as plt
-from inspect_ai.analysis import evals_df, samples_df
+from inspect_ai.analysis import evals_df
 
 df_eval = evals_df('../logs')
-# df_samples = samples_df('../logs')
 
-# %%
-print(df_eval.columns)
 df_eval.head()
+print(df_eval.columns)
 print(f"df_eval.shape: {df_eval.shape}")
 
-# %%
-task_names = df_eval['task_name'].unique()
-print(task_names)
-# %%
+task_name = 'starsim_agent_benchmark'
+df_task = df_eval[df_eval['task_name'] == task_name]
+df_task = df_task.sort_values(by='model')
+
+fig = plt.figure(figsize=(6,6))
+ax = plt.gca()
+ax.bar(np.arange(len(df_task)),df_task['score_headline_value'], color='xkcd:lightblue')
+ax.set_xticks(np.arange(len(df_task)))
+ax.set_xticklabels(df_task['model'], rotation=45)
+ax.set_ylim(0, 1)
+ax.set_xlabel('Model')
+ax.set_ylabel('Pass rate')
+ax.set_title('Starsim LLM Benchmark')
+
+sc.figlayout()
+
+plt.show()
+
+
 # # Figure 1: Pass rate vs. allowed time to solve per problem
 # task_name = 'starsim_agent_benchmark'
 # df_task = df_eval[df_eval['task_name'] == task_name]
@@ -35,26 +47,3 @@ print(task_names)
 # ax.set_xlabel('Allowed time to solve per problem (seconds)')
 # ax.set_ylabel('Pass rate')
 # ax.set_title('Starsim Agent Benchmark')
-
-# %%
-task_name = 'starsim_agent_benchmark'
-df_task = df_eval[df_eval['task_name'] == task_name]
-print('HI')
-print(df_task['task_name'].unique())
-print('OK')
-df_task = df_task.sort_values(by='model')
-print(df_task)
-print('and')
-sc.dataframe(df_task).disp()
-
-fig = plt.figure(figsize=(5,3))
-ax = plt.gca()
-ax.bar(np.arange(len(df_task)),df_task['score_headline_value'], color='xkcd:lightblue')
-ax.set_xticks(np.arange(len(df_task)))
-ax.set_xticklabels(df_task['model'], rotation=45)
-ax.set_ylim(0, 1)
-ax.set_xlabel('Model')
-ax.set_ylabel('Pass rate')
-ax.set_title('Starsim LLM Benchmark')
-
-plt.show()
